dee5f8dc4df8835f65c5abff16681efe63bf8333
Yan Tavares <yantdo1@gmail.com>
2023-04-18 17:50:31 -0300

    04 done

--- src/tutorial/01-useState/starter/04-useState-object.jsx
+++ src/tutorial/01-useState/starter/04-useState-object.jsx
@@ -1,7 +1,27 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [person, setPerson] = useState({
+    name: "Yan",
+    age: 21,
+    hobby: "To sleep :(",
+  });
+
+  const displayPerson = () => {
+    setPerson({ name: "Gui", age: 25, hobby: "Programming on free time" });
+
+    //? setPerson({ ...person, name:"gui" }); ---> Copia os atributos passados de person e da um overwrite na propriedade nome
+  };
+  return (
+    <>
+      <h3>{person.name}</h3>
+      <h3>Age: {person.age}</h3>
+      <h4>Enjoys: {person.hobby}</h4>
+      <button className="btn" onClick={displayPerson}>
+        Change
+      </button>
+    </>
+  );
 };
 
 export default UseStateObject;
