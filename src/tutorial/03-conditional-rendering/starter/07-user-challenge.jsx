67c6a55e86ae53c2ff4076679decb869a38ab0e0
Yan Tavares <yantdo1@gmail.com>
2023-04-27 17:29:22 -0300

    07 user challenge

--- src/tutorial/03-conditional-rendering/starter/07-user-challenge.jsx
+++ src/tutorial/03-conditional-rendering/starter/07-user-challenge.jsx
@@ -1,5 +1,47 @@
+import { useState, useEffect } from "react";
+
 const UserChallenge = () => {
-  return <h2>user challenge</h2>;
+  const [user, setUser] = useState(null);
+
+  const Logged = (props) => {
+    const { name } = props;
+    return (
+      <>
+        <h1>Hello there {name}!</h1>
+        <button
+          className="btn"
+          onClick={() => {
+            setUser(null);
+          }}
+        >
+          Log out
+        </button>
+      </>
+    );
+  };
+
+  const NotLogged = () => {
+    return (
+      <>
+        <h1>Please log in</h1>
+        <button
+          className="btn"
+          onClick={() => {
+            setUser({ name: "Yan" });
+          }}
+        >
+          Log in
+        </button>
+      </>
+    );
+  };
+
+  return (
+    <>
+      <h2>user challenge</h2>
+      {user ? <Logged {...user} /> : <NotLogged />}
+    </>
+  );
 };
 
 export default UserChallenge;
