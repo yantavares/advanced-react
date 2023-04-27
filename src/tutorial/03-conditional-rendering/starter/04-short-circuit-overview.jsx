c68926c42460912ed0416744cf4c7ccb938339cb
Yan Tavares <yantdo1@gmail.com>
2023-04-27 16:24:10 -0300

    short circuit end

--- src/tutorial/03-conditional-rendering/starter/04-short-circuit-overview.jsx
+++ src/tutorial/03-conditional-rendering/starter/04-short-circuit-overview.jsx
@@ -3,18 +3,24 @@ import { useState } from "react";
 const ShortCircuitOverview = () => {
   const [text, setText] = useState("");
   const [name, setName] = useState("Yan");
+  const [isEditing, setIsEditing] = useState(false);
 
   return (
     <>
-      <h2>short circuit overview</h2>
-      <h4>Falsy OR : {text || "Hello world"} </h4>
-      <h4>Truthy OR : {name || "Hello world"} </h4>
+      <div>
+        <h2>short circuit overview</h2>
+        <h4>Falsy OR : {text || "Hello world"} </h4>
+        <h4>Truthy OR : {name || "Hello world"} </h4>
+        <h4>Falsy AND : {text && "Hello world"} </h4>
+        <h4>Truthy AND : {name && "Hello world"} </h4>
+        {name && <div> This works! </div>}
+        {!text && <div> This also works! (see code) </div>}
+      </div>
 
-      <h4>Falsy AND : {text && "Hello world"} </h4>
-      <h4>Truthy AND : {name && "Hello world"} </h4>
-
-      {name && <div> This works! </div>}
-      {!text && <div> This also works! </div>}
+      <div>
+        <h2>Ternary operator</h2>
+        <button className="btn">{isEditing ? "edit" : "add"}</button>
+      </div>
     </>
   );
 };
