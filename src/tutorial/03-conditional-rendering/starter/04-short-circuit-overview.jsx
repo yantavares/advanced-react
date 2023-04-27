fca9b553c0ad6782bae1fd46680a18c665e6284e
Yan Tavares <yantdo1@gmail.com>
2023-04-27 16:19:26 -0300

    04 short circuit

--- src/tutorial/03-conditional-rendering/starter/04-short-circuit-overview.jsx
+++ src/tutorial/03-conditional-rendering/starter/04-short-circuit-overview.jsx
@@ -1,6 +1,21 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 const ShortCircuitOverview = () => {
-  return <h2>short circuit overview</h2>;
+  const [text, setText] = useState("");
+  const [name, setName] = useState("Yan");
+
+  return (
+    <>
+      <h2>short circuit overview</h2>
+      <h4>Falsy OR : {text || "Hello world"} </h4>
+      <h4>Truthy OR : {name || "Hello world"} </h4>
+
+      <h4>Falsy AND : {text && "Hello world"} </h4>
+      <h4>Truthy AND : {name && "Hello world"} </h4>
+
+      {name && <div> This works! </div>}
+      {!text && <div> This also works! </div>}
+    </>
+  );
 };
 export default ShortCircuitOverview;
