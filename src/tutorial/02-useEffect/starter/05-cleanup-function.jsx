504efdc16242f19d16cdcc9e48943b12c12f6071
Yan Tavares <yantdo1@gmail.com>
2023-04-28 09:04:33 -0300

    cleanup function

--- src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
+++ src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
@@ -1,5 +1,37 @@
+import { useEffect, useState } from "react";
+
 const CleanupFunction = () => {
-  return <h2>cleanup function</h2>;
+  const [tog, setTog] = useState(false);
+
+  return (
+    <>
+      <h2>cleanup function</h2>
+      <button
+        onClick={() => {
+          setTog(!tog);
+        }}
+        className="btn"
+      >
+        Toggle
+      </button>
+      {tog && <Comp />}
+    </>
+  );
+};
+
+const Comp = () => {
+  useEffect(() => {
+    console.log("This should load only once :(");
+    const intId = setInterval(() => {
+      console.log("This keeps on loading!");
+    }, 1000);
+
+    return () => {
+      clearInterval(intId);
+      console.log("Cleanup function");
+    };
+  }, []);
+  return <h3>Test</h3>;
 };
 
 export default CleanupFunction;
